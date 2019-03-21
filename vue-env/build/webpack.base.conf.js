55efb5274e459f2ecffef2056c26b32310c19db4
Lucas <everlastlucas@outlook.com>
2019-03-21 22:14:59 +0800

    fix: 修正配置

--- vue-env/build/webpack.base.conf.js
+++ vue-env/build/webpack.base.conf.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
@@ -103,7 +102,6 @@ module.exports = {
       filename: 'index.html',
       template: path.resolve(__dirname, '../public', 'index.html'),
     }),
-    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: 'css/[name].[hash].css',
     }),
--- vue-env/build/webpack.prod.conf.js
+++ vue-env/build/webpack.prod.conf.js
@@ -1,6 +1,11 @@
 const baseConfig = require('./webpack.base.conf');
 const merge = require('webpack-merge');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 
 module.exports = merge(baseConfig, {
   mode: 'production',
+  plugins: [
+    new CleanWebpackPlugin(),
+  ]
 });
